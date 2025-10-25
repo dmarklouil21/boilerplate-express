3e3fb03eb55e1b48729d5fb5a9c3154e8d208250
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-10-26 07:38:25 +0800

    fix text case issue

--- myApp.js
+++ myApp.js
@@ -3,8 +3,11 @@ let app = express();
 
 console.log("Hello World");
 
-// Serve static files from the "public" folder
-app.use(express.static(__dirname + '/public'));
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   let absolutePath = __dirname + '/views/index.html';
