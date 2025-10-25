08c82323d8e89153c5662ac235efef728099d2ae
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-10-26 07:08:13 +0800

    serve static assets

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", function (req, res) {
+
+  // Serve static files from the "public" folder
+  app.use(express.static(__dirname + '/public'));
   let absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
 });
