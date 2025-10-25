aa8af64876403f49f373b224d321bc46291bd2cb
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-10-26 07:46:57 +0800

    serve json on a specific route

--- myApp.js
+++ myApp.js
@@ -4,14 +4,16 @@ let app = express();
 console.log("Hello World");
 
 // Normal usage
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
 // Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
+// app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-  let absolutePath = __dirname + '/views/index.html';
-  res.sendFile(absolutePath);
+app.get("/json", function (req, res) {
+  // let absolutePath = __dirname + '/views/index.html';
+  // res.sendFile(absolutePath);
+
+  res.json({ message: "Hello json" });
 });
 
 
